ff6d5761a85d16f7d751505ca430acb68678f2e5
ichangho <ch.lee@rootenergy.co.kr>
2022-01-21 00:24:47 +0900

    fix rest api

--- index.js
+++ index.js
@@ -9,79 +9,79 @@ app.use(express.urlencoded({ extended: true }));
 //임시 데이터
 const infos = [
 	{
-		id: 1,
+		name: 'evil-dead-2013',
 		title: 'Evil Dead 2013',
 		detail:
 			'Evil Dead 2013에 관한 설명입니다. 아주 재미있는 영화입니다. 추천합니다. 두번보세요. 세번보세요',
 	},
 	{
-		id: 2,
+		name: 'annabelle-creation',
 		title: 'Annabelle Creation (2017)',
 		detail:
 			'Annabelle Creation (2017)에 관한 설명입니다. 아주 재미있는 영화입니다. 추천합니다. 두번보세요. 세번보세요',
 	},
 	{
-		id: 3,
+		name: 'insidious-the-last-key',
 		title: 'Insidious The Last Key (2018)',
 		detail:
 			'Insidious The Last Key (2018)에 관한 설명입니다. 아주 재미있는 영화입니다. 추천합니다. 두번보세요. 세번보세요',
 	},
 	{
-		id: 4,
+		name: 'the-conjuring-2',
 		title: 'The Conjuring 2 2016',
 		detail:
 			'The Conjuring 2 2016에 관한 설명입니다. 아주 재미있는 영화입니다. 추천합니다. 두번보세요. 세번보세요',
 	},
 	{
-		id: 5,
+		name: 'dawn-of-justice',
 		title: 'Dawn of Justice',
 		detail:
 			'Dawn of Justice에 관한 설명입니다. 아주 재미있는 영화입니다. 추천합니다. 두번보세요. 세번보세요',
 	},
 	{
-		id: 6,
+		name: 'suicide-squad',
 		title: 'Suicide Squad',
 		detail:
 			'Suicide Squad에 관한 설명입니다. 아주 재미있는 영화입니다. 추천합니다. 두번보세요. 세번보세요',
 	},
 	{
-		id: 7,
+		name: 'john-wick-chapter-1',
 		title: 'John Wick Chapter 1',
 		detail:
 			'John Wick Chapter 1에 관한 설명입니다. 아주 재미있는 영화입니다. 추천합니다. 두번보세요. 세번보세요',
 	},
 	{
-		id: 8,
+		name: 'john-wick-chapter-2',
 		title: 'John Wick Chapter 2',
 		detail:
 			'John Wick Chapter 2에 관한 설명입니다. 아주 재미있는 영화입니다. 추천합니다. 두번보세요. 세번보세요',
 	},
 	{
-		id: 9,
+		name: 'john-wick-chapter-3',
 		title: 'John Wick Chapter 3',
 		detail:
 			'John Wick Chapter 3에 관한 설명입니다. 아주 재미있는 영화입니다. 추천합니다. 두번보세요. 세번보세요',
 	},
 	{
-		id: 10,
+		name: 'lost-in-space-s1',
 		title: 'Lost In Space S1',
 		detail:
 			'Lost In Space S1에 관한 설명입니다. 아주 재미있는 영화입니다. 추천합니다. 두번보세요. 세번보세요',
 	},
 	{
-		id: 11,
+		name: 'lost-in-space-s2',
 		title: 'Lost In Space S2',
 		detail:
 			'Lost In Space S2에 관한 설명입니다. 아주 재미있는 영화입니다. 추천합니다. 두번보세요. 세번보세요',
 	},
 	{
-		id: 12,
+		name: 'pacific-rim-2013',
 		title: 'Pacific Rim 2013',
 		detail:
 			'Pacific Rim 2013에 관한 설명입니다. 아주 재미있는 영화입니다. 추천합니다. 두번보세요. 세번보세요',
 	},
 	{
-		id: 13,
+		name: 'pacific-rim-uprising-2017',
 		title: 'Pacific Rim Uprising 2017',
 		detail:
 			'Pacific Rim Uprising 2017에 관한 설명입니다. 아주 재미있는 영화입니다. 추천합니다. 두번보세요. 세번보세요',
@@ -93,32 +93,36 @@ app.get('/', (req, res) => {
 	res.send('Hello World');
 });
 
-app.get('/api/detail', (req, res) => {
-	const id = req.query.id;
-	const info = infos.filter((data) => Number(data.id) === Number(id));
+app.get('/api/movie', (req, res) => {
+	res.json({ infos });
+});
+
+app.get('/api/movie/:moviename', (req, res) => {
+	const moviename = req.params.moviename;
+	const info = infos.filter((data) => data.name === moviename);
 	console.log('====================================');
 	console.log(info);
 	console.log('====================================');
 	res.json({ info });
 });
 
-app.post('/api/detail', (req, res) => {
-	const { id, title, detail } = req.body;
-	const info = infos.concat({ id, title, detail });
+app.post('/api/movie/:moviename', (req, res) => {
+	const moviename = req.params.moviename;
+	const { title, detail } = req.body;
+	const info = infos.concat({ moviename, title, detail });
 	res.json({ info });
 });
 
-app.put('/api/detail', (req, res) => {
-	const { id, title, detail } = req.body;
+app.put('/api/movie/:moviename', (req, res) => {
+	const moviename = req.params.moviename;
+	const { title, detail } = req.body;
 	const info = infos.map((data) => {
-		if (data.id == id) {
-			if (data.id == id) {
-				data.title = title;
-				data.detail = detail;
-			}
+		if (data.name == moviename) {
+			data.title = title;
+			data.detail = detail;
 		}
 		return {
-			id: data.id,
+			name: data.name,
 			title: data.title,
 			detail: data.detail,
 		};
@@ -126,17 +130,16 @@ app.put('/api/detail', (req, res) => {
 	res.json({ info });
 });
 
-app.patch('/api/detail', (req, res) => {
-	const { id, title, detail } = req.body;
+app.patch('/api/movie/:moviename', (req, res) => {
+	const moviename = req.params.moviename;
+	const { title, detail } = req.body;
 	const info = infos.map((data) => {
-		if (data.id == id) {
-			if (data.id == id) {
-				data.title = title;
-				data.detail = detail;
-			}
+		if (data.name == moviename) {
+			data.title = title;
+			data.detail = detail;
 		}
 		return {
-			id: data.id,
+			name: data.name,
 			title: data.title,
 			detail: data.detail,
 		};
@@ -144,9 +147,9 @@ app.patch('/api/detail', (req, res) => {
 	res.json({ info });
 });
 
-app.delete('/api/detail', (req, res) => {
-	const id = req.query.id;
-	const info = infos.filter((data) => data.id != id);
+app.delete('/api/movie/:moviename', (req, res) => {
+	const moviename = req.params.moviename;
+	const info = infos.filter((data) => data.name != moviename);
 	res.json({ info });
 });
 
